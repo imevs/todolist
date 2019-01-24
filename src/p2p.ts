ca4202248a8f98be653d9e852ab517e588f2da85
vitaly.medvedev <medvedev.vs@gmail.com>
2019-01-25 01:20:29 +0200

    Display connection status by address bar color

--- src/p2p.ts
+++ src/p2p.ts
@@ -157,18 +157,24 @@ connection.oniceconnectionstatechange = () => {
 
 function setConnectedStatus() {
     console.log("setConnectedStatus");
+    const bar = document.querySelector("[name=theme-color]") as HTMLMetaElement;
+    bar.content = "green";
     const favicon = document.getElementById('favicon') as HTMLLinkElement;
     favicon.href = "circle-green.png";
 }
 
 function setDisconnectedStatus() {
     console.log("setDisconnectedStatus");
+    const bar = document.querySelector("[name=theme-color]") as HTMLMetaElement;
+    bar.content = "red";
     const favicon = document.getElementById('favicon') as HTMLLinkElement;
     favicon.href = "circle-red.png";
 }
 
 function setConnectingStatus() {
     console.log("setConnectingStatus");
+    const bar = document.querySelector("[name=theme-color]") as HTMLMetaElement;
+    bar.content = "orange";
     const favicon = document.getElementById('favicon') as HTMLLinkElement;
     favicon.href = "circle-orange.png";
 }
